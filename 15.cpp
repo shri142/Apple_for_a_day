a437634ffa092f9305af8ce17fd6a3efa41afefd
Shrikrishna umbare <81535914+shri142@users.noreply.github.com>
2022-12-31 22:42:49 +0530

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	
+	cin>>T;
+	
+	for(int i=0;i<T;i++){
+	    int a,b,a1,b1,a2,b2;
+	    
+	    cin>>a>>b>>a1>>b1>>a2>>b2;
+	    
+	    
+	    if ( ((a==a1)&&(b==b1))||((a==b1)&&(b==a1)) ){
+	        cout<<1<<endl;
+	    }
+	    else if ( ((a==a2)&&(b==b2))||((a==b2)&&(b==a2)) ){
+	        cout<<2<<endl;
+	    }
+	    else{
+	        cout<<0<<endl;
+	    }
+	    
+	       
+	}
+	
+	
+	return 0;
+}
