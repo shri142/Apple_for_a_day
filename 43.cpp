032bddb6342160b2ff98d25203675c41ad03e79b
Shrikrishna umbare <81535914+shri142@users.noreply.github.com>
2023-01-28 23:48:47 +0530

    Create 43.cpp

---
+++ 43.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int arr[4];
+	int count=0;
+	for(int i=0;i<4;i++)
+	{
+	    cin>>arr[i];
+	    if(arr[i]>=10)
+	    {
+	        count++;
+	    }
+	}
+	cout<<count<<endl;
+	return 0;
+}
